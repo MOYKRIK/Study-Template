797401cb41ea3c4b59cc71531ee5d7e58905f970
Артем Сидоров <Sdrv2007@gmail.com>
2026-04-11 14:14:07 +0300

    [Feature] Test

---
+++ src/Laba1/Study.LabWork1.UnitTests/Features/Task1/MySet.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.LabWork1.UnitTests.Features.Task1
+{
+    [TestFixture]
+    internal class MySet
+    {
+    }
+}
---
+++ src/Laba1/Study.LabWork1/Features/Task1/MySet.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.LabWork1.Features.Task1
+{
+    internal class MySet
+    {
+
+    }
+}
