c71855bd8222db49dc969b4a488c965c38840ace
Артем Сидоров <Sdrv2007@gmail.com>
2026-05-03 19:30:03 +0300

    [Feature] Выполнил задание 1.1 и написал тесты

--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MutexServiceTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MutexServiceTests.cs
@@ -1,6 +1,40 @@
+using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
+using Study.LabWork2.Feature.Task1.SubTask1;
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask1;
 
 [TestFixture]
 public sealed class MutexServiceTests
 {
+    private IPrimeCounter _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new MutexService();
+    }
+
+    [Test]
+    public void CountPrimes_CorrectTotal()
+    {
+        var result = _service.CountPrimes(1, 100, 4);
+
+        Assert.That(result.PrimeCount, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void CountPrimes_ConsistentResults()
+    {
+        var r1 = _service.CountPrimes(1, 500, 4);
+        var r2 = _service.CountPrimes(1, 500, 4);
+
+        Assert.That(r1.PrimeCount, Is.EqualTo(r2.PrimeCount));
+    }
+
+    [Test]
+    public void CountPrimes_DoesNotDeadlock()
+    {
+        Assert.DoesNotThrow(() =>
+            _service.CountPrimes(1, 200, 4)
+        );
+    }
 }
--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/SemaphoreServiceTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/SemaphoreServiceTests.cs
@@ -1,6 +1,41 @@
+using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
+using Study.LabWork2.Feature.Task1.SubTask1;
+
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask1;
 
 [TestFixture]
 public sealed class SemaphoreServiceTests
 {
+    private IPrimeCounter _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new SemaphoreService();
+    }
+
+    [Test]
+    public void CountPrimes_CorrectResult()
+    {
+        var result = _service.CountPrimes(1, 100, 4);
+
+        Assert.That(result.PrimeCount, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void CountPrimes_ThreadLimitDoesNotBreakLogic()
+    {
+        var result = _service.CountPrimes(1, 10000, 4);
+
+        Assert.That(result.PrimeCount, Is.EqualTo(1229));
+    }
+
+    [Test]
+    public void CountPrimes_RepeatedRunsStable()
+    {
+        var r1 = _service.CountPrimes(1, 1000, 4);
+        var r2 = _service.CountPrimes(1, 1000, 4);
+
+        Assert.That(r1.PrimeCount, Is.EqualTo(r2.PrimeCount));
+    }
 }
